349da78230c838127b91ab40e70da07b5cd2777f
maxmono <payza2013@gmail.com>
2018-04-26 20:34:00 +0200

    Interface #1
    This is the first Interface addition
    
    it contains:
    -log in form
    -Main form
    - Sales form

---
+++ ExoCasualWear/ExoCasualWear/Main.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace ExoCasualWear
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.salesbutton = new System.Windows.Forms.Button();
+            this.reportsbutton = new System.Windows.Forms.Button();
+            this.Inventorybutton = new System.Windows.Forms.Button();
+            this.closetheshiftbutton = new System.Windows.Forms.Button();
+            this.exitbutton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recoveryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createBackupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadBackupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemsListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.analysisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salesAnalysisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemsFrequencyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timeAnalysisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // salesbutton
+            // 
+            this.salesbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salesbutton.Location = new System.Drawing.Point(12, 667);
+            this.salesbutton.Name = "salesbutton";
+            this.salesbutton.Size = new System.Drawing.Size(323, 104);
+            this.salesbutton.TabIndex = 0;
+            this.salesbutton.Text = "Sales";
+            this.salesbutton.UseVisualStyleBackColor = true;
+            this.salesbutton.Click += new System.EventHandler(this.salesbutton_Click);
+            // 
+            // reportsbutton
+            // 
+            this.reportsbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reportsbutton.Location = new System.Drawing.Point(344, 667);
+            this.reportsbutton.Name = "reportsbutton";
+            this.reportsbutton.Size = new System.Drawing.Size(323, 104);
+            this.reportsbutton.TabIndex = 1;
+            this.reportsbutton.Text = "Reports";
+            this.reportsbutton.UseVisualStyleBackColor = true;
+            // 
+            // Inventorybutton
+            // 
+            this.Inventorybutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Inventorybutton.Location = new System.Drawing.Point(676, 667);
+            this.Inventorybutton.Name = "Inventorybutton";
+            this.Inventorybutton.Size = new System.Drawing.Size(323, 104);
+            this.Inventorybutton.TabIndex = 2;
+            this.Inventorybutton.Text = "Inventory";
+            this.Inventorybutton.UseVisualStyleBackColor = true;
+            // 
+            // closetheshiftbutton
+            // 
+            this.closetheshiftbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closetheshiftbutton.Location = new System.Drawing.Point(1008, 667);
+            this.closetheshiftbutton.Name = "closetheshiftbutton";
+            this.closetheshiftbutton.Size = new System.Drawing.Size(323, 104);
+            this.closetheshiftbutton.TabIndex = 3;
+            this.closetheshiftbutton.Text = "Close the shift";
+            this.closetheshiftbutton.UseVisualStyleBackColor = true;
+            // 
+            // exitbutton
+            // 
+            this.exitbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitbutton.Location = new System.Drawing.Point(1189, 12);
+            this.exitbutton.Name = "exitbutton";
+            this.exitbutton.Size = new System.Drawing.Size(137, 83);
+            this.exitbutton.TabIndex = 4;
+            this.exitbutton.Text = "Exit";
+            this.exitbutton.UseVisualStyleBackColor = true;
+            this.exitbutton.Click += new System.EventHandler(this.exitbutton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mainToolStripMenuItem,
+            this.recoveryToolStripMenuItem,
+            this.itemsToolStripMenuItem,
+            this.analysisToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1338, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mainToolStripMenuItem
+            // 
+            this.mainToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.logOutToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.mainToolStripMenuItem.Name = "mainToolStripMenuItem";
+            this.mainToolStripMenuItem.Size = new System.Drawing.Size(54, 24);
+            this.mainToolStripMenuItem.Text = "Main";
+            // 
+            // logOutToolStripMenuItem
+            // 
+            this.logOutToolStripMenuItem.Name = "logOutToolStripMenuItem";
+            this.logOutToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.logOutToolStripMenuItem.Text = "Log out";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // recoveryToolStripMenuItem
+            // 
+            this.recoveryToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createBackupToolStripMenuItem,
+            this.loadBackupToolStripMenuItem});
+            this.recoveryToolStripMenuItem.Name = "recoveryToolStripMenuItem";
+            this.recoveryToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.recoveryToolStripMenuItem.Text = "Recovery";
+            // 
+            // createBackupToolStripMenuItem
+            // 
+            this.createBackupToolStripMenuItem.Name = "createBackupToolStripMenuItem";
+            this.createBackupToolStripMenuItem.Size = new System.Drawing.Size(173, 24);
+            this.createBackupToolStripMenuItem.Text = "Create backup";
+            // 
+            // loadBackupToolStripMenuItem
+            // 
+            this.loadBackupToolStripMenuItem.Name = "loadBackupToolStripMenuItem";
+            this.loadBackupToolStripMenuItem.Size = new System.Drawing.Size(173, 24);
+            this.loadBackupToolStripMenuItem.Text = "Load backup";
+            // 
+            // itemsToolStripMenuItem
+            // 
+            this.itemsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itemsListToolStripMenuItem,
+            this.addItemToolStripMenuItem,
+            this.editItemToolStripMenuItem});
+            this.itemsToolStripMenuItem.Name = "itemsToolStripMenuItem";
+            this.itemsToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.itemsToolStripMenuItem.Text = "Items";
+            // 
+            // itemsListToolStripMenuItem
+            // 
+            this.itemsListToolStripMenuItem.Name = "itemsListToolStripMenuItem";
+            this.itemsListToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.itemsListToolStripMenuItem.Text = "Items list";
+            // 
+            // addItemToolStripMenuItem
+            // 
+            this.addItemToolStripMenuItem.Name = "addItemToolStripMenuItem";
+            this.addItemToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.addItemToolStripMenuItem.Text = "Add Item";
+            // 
+            // editItemToolStripMenuItem
+            // 
+            this.editItemToolStripMenuItem.Name = "editItemToolStripMenuItem";
+            this.editItemToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.editItemToolStripMenuItem.Text = "Edit Item";
+            // 
+            // analysisToolStripMenuItem
+            // 
+            this.analysisToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salesAnalysisToolStripMenuItem,
+            this.itemsFrequencyToolStripMenuItem,
+            this.timeAnalysisToolStripMenuItem});
+            this.analysisToolStripMenuItem.Name = "analysisToolStripMenuItem";
+            this.analysisToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.analysisToolStripMenuItem.Text = "Analysis";
+            // 
+            // salesAnalysisToolStripMenuItem
+            // 
+            this.salesAnalysisToolStripMenuItem.Name = "salesAnalysisToolStripMenuItem";
+            this.salesAnalysisToolStripMenuItem.Size = new System.Drawing.Size(185, 24);
+            this.salesAnalysisToolStripMenuItem.Text = "Sales Analysis";
+            // 
+            // itemsFrequencyToolStripMenuItem
+            // 
+            this.itemsFrequencyToolStripMenuItem.Name = "itemsFrequencyToolStripMenuItem";
+            this.itemsFrequencyToolStripMenuItem.Size = new System.Drawing.Size(185, 24);
+            this.itemsFrequencyToolStripMenuItem.Text = "Items Frequency";
+            // 
+            // timeAnalysisToolStripMenuItem
+            // 
+            this.timeAnalysisToolStripMenuItem.Name = "timeAnalysisToolStripMenuItem";
+            this.timeAnalysisToolStripMenuItem.Size = new System.Drawing.Size(185, 24);
+            this.timeAnalysisToolStripMenuItem.Text = "Time Analysis";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::ExoCasualWear.Properties.Resources.Exo_logo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1338, 783);
+            this.Controls.Add(this.exitbutton);
+            this.Controls.Add(this.closetheshiftbutton);
+            this.Controls.Add(this.Inventorybutton);
+            this.Controls.Add(this.reportsbutton);
+            this.Controls.Add(this.salesbutton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "Exo Casual Wear";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button salesbutton;
+        private System.Windows.Forms.Button reportsbutton;
+        private System.Windows.Forms.Button Inventorybutton;
+        private System.Windows.Forms.Button closetheshiftbutton;
+        private System.Windows.Forms.Button exitbutton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logOutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recoveryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createBackupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadBackupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itemsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itemsListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addItemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editItemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem analysisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salesAnalysisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itemsFrequencyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem timeAnalysisToolStripMenuItem;
+    }
+}
---
+++ ExoCasualWear/ExoCasualWear/Sales.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ExoCasualWear
+{
+    partial class Sales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.barcodeentrytextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.printbutton = new System.Windows.Forms.Button();
+            this.minusbutton = new System.Windows.Forms.Button();
+            this.plusbutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(105, 173);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(360, 594);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // barcodeentrytextbox
+            // 
+            this.barcodeentrytextbox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.barcodeentrytextbox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.HistoryList;
+            this.barcodeentrytextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.barcodeentrytextbox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.barcodeentrytextbox.Location = new System.Drawing.Point(741, 198);
+            this.barcodeentrytextbox.Name = "barcodeentrytextbox";
+            this.barcodeentrytextbox.Size = new System.Drawing.Size(378, 45);
+            this.barcodeentrytextbox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(572, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Barcode:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // printbutton
+            // 
+            this.printbutton.BackgroundImage = global::ExoCasualWear.Properties.Resources.print;
+            this.printbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.printbutton.Font = new System.Drawing.Font("Arial Narrow", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.printbutton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.printbutton.Location = new System.Drawing.Point(12, 348);
+            this.printbutton.Name = "printbutton";
+            this.printbutton.Size = new System.Drawing.Size(75, 70);
+            this.printbutton.TabIndex = 5;
+            this.printbutton.UseVisualStyleBackColor = true;
+            // 
+            // minusbutton
+            // 
+            this.minusbutton.BackgroundImage = global::ExoCasualWear.Properties.Resources.minus;
+            this.minusbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.minusbutton.Font = new System.Drawing.Font("Arial Narrow", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minusbutton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.minusbutton.Location = new System.Drawing.Point(12, 262);
+            this.minusbutton.Name = "minusbutton";
+            this.minusbutton.Size = new System.Drawing.Size(75, 70);
+            this.minusbutton.TabIndex = 4;
+            this.minusbutton.UseVisualStyleBackColor = true;
+            // 
+            // plusbutton
+            // 
+            this.plusbutton.BackgroundImage = global::ExoCasualWear.Properties.Resources.plus;
+            this.plusbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.plusbutton.Font = new System.Drawing.Font("Arial Narrow", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusbutton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.plusbutton.Location = new System.Drawing.Point(12, 173);
+            this.plusbutton.Name = "plusbutton";
+            this.plusbutton.Size = new System.Drawing.Size(75, 70);
+            this.plusbutton.TabIndex = 3;
+            this.plusbutton.UseVisualStyleBackColor = true;
+            // 
+            // Sales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1319, 779);
+            this.Controls.Add(this.printbutton);
+            this.Controls.Add(this.minusbutton);
+            this.Controls.Add(this.plusbutton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.barcodeentrytextbox);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Sales";
+            this.Text = "Sales";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox barcodeentrytextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button plusbutton;
+        private System.Windows.Forms.Button minusbutton;
+        private System.Windows.Forms.Button printbutton;
+    }
+}
