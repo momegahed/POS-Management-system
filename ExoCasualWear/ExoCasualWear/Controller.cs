00d651be3aa03135aa27afa40dc5cdbe7fcca8f9
Abdallah-Shawky <37694200+Abdallah-Shawky@users.noreply.github.com>
2018-04-29 03:21:11 +0200

    Three functions are added to controller . these are:1) item profile, 2)Member profile,3)Shipment profile

--- ExoCasualWear/ExoCasualWear/Controller.cs
+++ ExoCasualWear/ExoCasualWear/Controller.cs
@@ -37,16 +37,39 @@ namespace ExoCasualWear
 
             return dbMan.UpdateData(query);
         }
-        public int CalcSales(int Discount_price)
+        public int CalcSales()
         {
             string query = " SELECT Sum(Discount_price) AS Profit FROM Receipt";
             return dbMan.UpdateData(query);
         }
 
-        public int CalcEmp(string ID)
+        public int CalcEmp()
         {
-            string query = " SELECT Sum(ID) AS  FROM Employee";
+            string query = " SELECT Count(ID) AS NumOfEmp FROM Employee";
             return dbMan.UpdateData(query);
+        }
+
+        public DataTable ItemProfile(string ItemNum)
+        {
+            string query = "SELECT Item_discription, Category, Item_size, Color, Brand, Offer.Value, O_Start, O_End, SoldAt.Available, SoldAt.StID, Store.St_City, Store.St_State  FROM Items, Offer, SoldAt, Store WHERE ItemNO#='" + ItemNum + "' AND Items.OfID=Offer.OfferID AND SoldAt.ItemsNumber=Items.ItemNO# AND SoldAt.StID=Store.Store#;";
+            return dbMan.ExecuteTableQuery(query);
+        }
+
+        public DataTable MemberProfile(string MemberNum)
+        {
+            string query = "SELECT C_Fname, C_Lname, C_Phone#, Receipt.Total_price, Receipt.Discount_price, R_Contains.Quantity, R_Contains.ReceiptID, R_Contains.Item#No, Items.Item_discription, Offer.Value FROM Customer, Receipt, R_Contains, Items, Offer WHERE MembershipID#='" + MemberNum + "' AND Receipt.MemID#=Customer.MembershipID# AND R_Contains.ReceiptID=Receipt.Receipt# AND R_Contains.Item#No=Items.ItemNO# AND Items.OfID=Offer.OfferID;";
+            return dbMan.ExecuteTableQuery(query);
+        }
+
+        public DataTable ShipmentProfile(string ShipNum)
+        {
+            string query = "SELECT ShipmentID#, Sh_Date, Supplier.Su_Fname, Supplier.Su_Lname, Supplier.Su_Phone#, Supply_Store.ItemsNum, Supply_Store.StoreID, Supply_Store.Quantity, Items.Item_discription, Items.Price, Offer.Value FROM Shipment, Supplier, Supply_Store, Items, Offer WHERE ShipmentID#='" + ShipNum + "' AND Shipment.SupID#=Supplier.SupplierID# AND Supply_Store.ShipID=Shipment.ShipmentID# AND Supply_Store.ItemsNum=Items.ItemNO# AND Items.OfID=Offer.OfferID;";
+            return dbMan.ExecuteTableQuery(query);
+        }
+        public DataTable ShipmentProfile()
+        {
+            string query = "select stid, st_city, st_state, itemsnumber, item_discription as Item_name, max(sold) as #sold from soldAt s , store,  items where itemsnumber=itemNO# AND stid=store# group by stid, itemsnumber, st_city, st_state, item_discription having max(sold) = ( select top 1 max(sold) from soldAt where stid = s.stid group by stid, itemsnumber order by max(sold) desc);";
+            return dbMan.ExecuteTableQuery(query);
         }
     }
 }
