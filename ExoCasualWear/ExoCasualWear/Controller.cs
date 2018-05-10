1db1fb54175b767781742757706844e8ab7c151a
mhmdgad97 <mhmdgad97@users.noreply.github.com>
2018-05-11 00:22:21 +0200

    connecction sucsess

--- ExoCasualWear/ExoCasualWear/Controller.cs
+++ ExoCasualWear/ExoCasualWear/Controller.cs
@@ -20,13 +20,13 @@ namespace ExoCasualWear
 
         public int InsertEmployee(string ID, string E_Fname, string E_Lname, string E_City, string E_Street, string E_State, int St_Hours, string St_ID, string Super_ID)
         {
-            string query = "INSERT INTO Employee (ID, E_Fname, E_Lname, E_City, E_street, E_State, St_Hours, St_ID, Super_ID " +
-                            "Values ('" + ID + "','" + E_Fname + "','" + E_Lname + "'," + E_City + "'," + E_Street + "'," + E_State + "'," + St_Hours + "'," + St_ID + "'," + Super_ID + ");";
+            string query = "INSERT INTO Employee (ID, E_Fname, E_Lname, E_City, E_street, E_State, St_Hours, St_ID, SuperID ) " +
+                            "Values ('" + ID + "','" + E_Fname + "','" + E_Lname + "','" + E_City + "','" + E_Street + "','" + E_State + "','" + St_Hours + "','" + St_ID + "','" + Super_ID + "');";
             return Int16.Parse(dbMan.UpdateData(query).ToString());
         }
         public int InsertItems(string ItemNO, string Item_discription, int Price, string Category, string Item_size, string Color, int Brand, string OFID)
         {
-            string query = "INSERT INTO Items (ItemNO#, Item_Discription, Price, Category, Item_size, Color, Brand, OfID" +
+            string query = "INSERT INTO Items (ItemNO#, Item_Discription,Item_detailed_discription, Price, Brand, OfID)" +
                             "Values ('" + ItemNO + "','" + Item_discription + "','" + Price + "'," + Category + "'," + Item_size + "'," + Color + "'," + Brand + "'," + OFID + ");";
 
             return dbMan.UpdateData(query);
@@ -34,7 +34,7 @@ namespace ExoCasualWear
         public int InsertCustomer(string MembershipID, string C_Fname, string C_Lname, string C_Phone)
         {
             string query = "INSERT INTO Customer (MembershipID#, C_Fname, C_Lname, C_Phone#" +
-                            "Values ('" + MembershipID + "','" + C_Fname + "','" + C_Lname + "'," + C_Phone  + ");";
+                            "Values ('" + MembershipID + "','" + C_Fname + "','" + C_Lname + "','" + C_Phone  + "');";
 
             return dbMan.UpdateData(query);
         }
