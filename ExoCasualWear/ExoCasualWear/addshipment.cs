6ac93a534a5f056059bbcc9157a7c976a32a5251
maxmono <payza2013@gmail.com>
2018-05-12 23:56:25 +0200

    add shipment data gathered

--- ExoCasualWear/ExoCasualWear/addshipment.cs
+++ ExoCasualWear/ExoCasualWear/addshipment.cs
@@ -12,6 +12,9 @@ namespace ExoCasualWear
 {
     public partial class addshipment : Form
     {
+
+        Controller c = new Controller();
+
         public addshipment()
         {
             InitializeComponent();
@@ -23,6 +26,31 @@ namespace ExoCasualWear
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addshipment_Load(object sender, EventArgs e)
+        {
+            DataTable dt = c.Store_Details(); 
+            comboBox1.DataSource = dt;
+            comboBox1.DisplayMember = "St_Street";
+         //   comboBox1.ValueMember = "SupplierID#";
+           // dataGridView1.DataSource = bs;
+           // dataGridView1.Refresh();
+            // TODO: This line of code loads data into the 'exoDataSet1.Supplier' table. You can move, or remove it, as needed.
+            this.supplierTableAdapter.Fill(this.exoDataSet1.Supplier);
+            // TODO: This line of code loads data into the 'exoDataSet1.Supplier' table. You can move, or remove it, as needed.
+            this.supplierTableAdapter.Fill(this.exoDataSet1.Supplier);
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
